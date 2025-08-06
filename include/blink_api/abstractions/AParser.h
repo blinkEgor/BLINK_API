45210344789ec70d6682d0b9ac0f1ba661279584
blinkEgor <barminov44@gmail.com>
2025-08-06 11:16:02 +0300

    feat: Added AParser interface to include/blink_api/abstractions/AParser.h

---
+++ include/blink_api/abstractions/AParser.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include <string>
+#include <unordered_map>
+
+class AParser {
+public:
+	// Загрузка конфигурации из файла в unordered_map
+	virtual bool load_from_file( const std::string& path ) = 0;
+
+	// Сохранение конфигурации в файл из unordered_map
+	virtual bool save_to_file( const std::string& path ) const = 0;
+
+	// Получить значение по ключу с fallback из unordered_map
+	virtual std::string get( const std::string& key, const std::string& fallback = "" ) const = 0;
+
+	// Установить значение по ключу
+	virtual void set( const std::string& key, const std::string& value ) = 0;
+
+	// Получить карту всех пар ключ-значение
+	virtual std::unordered_map< std::string, std::string > get_all() const = 0;
+
+	virtual ~AParser() = default;
+};
