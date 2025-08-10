ac85d3a041fda3f3def159693c8166f2990cc733
blinkEgor <barminov44@gmail.com>
2025-08-10 11:25:07 +0300

    feat: The BLINK_API class has been created, which stores references to stubs for now and can update/init/exit.

--- include/blink_api/api.h
+++ include/blink_api/api.h
@@ -1,6 +1,24 @@
 #pragma once
 
-namespace BLINK_API {
+#include "abstractions/ALogger.h"
+#include "abstractions/AOutput.h"
+#include "abstractions/AScriptEngine.h"
+#include "abstractions/AParser.h"
+#include "abstractions/AInput.h"
+
+class BLINK_API {
+public:
+	BLINK_API();
+	~BLINK_API();
+
 	bool init();
+	void update();
 	bool exit();
-}
+
+private:
+	ALogger* logger;
+	AOutput* output;
+	AScriptEngine* script;
+	AParser* parser;
+	AInput* input;
+};
