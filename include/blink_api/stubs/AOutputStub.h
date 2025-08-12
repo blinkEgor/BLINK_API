9d72ac9f37f83cfbf5a7f0ea15d6f10af0b5b188
blinkEgor <barminov44@gmail.com>
2025-08-12 09:34:56 +0300

    feat: created header file include/blink_api/stubs/AOutputStub.h

---
+++ include/blink_api/stubs/AOutputStub.h
@@ -0,0 +1,9 @@
+#include "../abstractions/AOutput.h"
+
+class AOutputStub : public AOutput {
+public:
+	void cli_out( const std::string& message, text_color color, bg_color bg ) override;
+	void cli_endl();
+	bool file_out( const std::string& message, const std::string& path, text_color color, bg_color bg ) override;
+	bool file_endl( const std::string& path ) override;
+}
