19c2626b27029f8c4cc3cdcf9c703972b9a06e66
blinkEgor <barminov44@gmail.com>
2025-08-13 10:37:21 +0300

    feat: updated src/stubs/AScriptEngineStub.cpp

--- src/stubs/AScriptEngineStub.cpp
+++ src/stubs/AScriptEngineStub.cpp
@@ -1,17 +1,14 @@
-#include "../../include/blink_api/abstractions/AScriptEngine.h"
+#include "../../include/blink_api/stubs/AScriptEngine.h"
 #include <iostream>
 
-class AScriptEngineStub : public AScriptEngine {
-public:
-	void init() override {
-		std::cerr << "[TRACE] AScriptEngineStub::init() called (no script engine connected)" << std::endl;
-	}
+void AScriptEngine::init() {
+	std::cerr << "[TRACE] AScriptEngineStub::init() called (no script engine connected)" << std::endl;
+}
 
-	void update() override {
-		std::cerr << "[TRACE] AScriptEngineStub::update() called (no script logic)" << std::endl;
-	}
+void AScriptEngine::update() {
+	std::cerr << "[TRACE] AScriptEngineStub::update() called (no script logic)" << std::endl;
+}
 
-	void exit() override {
-		std::cerr << "[TRACE] AScriptEngineStub::exit() called (no cleanup required)" << std::endl;
-	}
-};
+void AScriptEngine::exit() {
+	std::cerr << "[TRACE] AScriptEngineStub::exit() called (no cleanup required)" << std::endl;
+}
