c9760878ad145fa78c6f2a1b49238c837a7340d2
blinkEgor <barminov44@gmail.com>
2025-08-14 13:37:05 +0300

    feat: updated src/stubs/AInputStub.cpp

--- src/stubs/AInputStub.cpp
+++ src/stubs/AInputStub.cpp
@@ -1,15 +1,12 @@
-#include "../../include/blink_api/abstractions/AInput.h"
+#include "../../include/blink_api/stubs/AInputStub.h"
 
-class AInputStub : public AInput {
-public:
-	std::string get_input( const std::string& message = ": " ) {
-		std::cerr << "[TRACE] " << "[AInputStub] " << "Get input is working\n";
+std::string AInputStub::get_input( const std::string& message = ": " ) {
+	std::cerr << "[TRACE] " << "[AInputStub] " << "Get input is working\n";
 
-		std::string hendle_input = "";
+	std::string hendle_input = "";
 
-		std::cerr << message;
-		std::cin >> hendle_input;
+	std::cerr << message;
+	std::cin >> hendle_input;
 
-		return hendle_input;
-	}
-};
+	return hendle_input;
+}
