6e6d23260320847a575c2c94cd17e29c9aa7f3d6
blinkEgor <barminov44@gmail.com>
2025-08-16 09:32:24 +0300

    feat(api): export C symbols with BLINK_API_EXPORT

--- include/blink_api/api.h
+++ include/blink_api/api.h
@@ -1,5 +1,12 @@
 #pragma once
 
+// --- макрос для экспорта символов ---
+#if defined(_WIN32) || defined(_WIN64)
+	#define BLINK_API_EXPORT __declspec(dllexport)
+#else
+	#define BLINK_API_EXPORT __attribute__((visibility("default")))
+#endif
+
 #include "abstractions/ALogger.h"
 #include "abstractions/AOutput.h"
 #include "abstractions/AScriptEngine.h"
@@ -26,11 +33,11 @@ private:
 // ==== C API ====
 // Эти функции будут вызываться при dlopen
 extern "C" {
-	BLINK_API* create_api();
-	void destroy_api( BLINK_API* api );
+	BLINK_API_EXPORT BLINK_API* create_api();
+	BLINK_API_EXPORT void destroy_api( BLINK_API* api );
 
 	// Обёртки для динамической загрузки:
-	bool api_init( BLINK_API* api );
-	void api_update( BLINK_API* api );
-	bool api_exit( BLINK_API* api );
+	BLINK_API_EXPORT bool api_init( BLINK_API* api );
+	BLINK_API_EXPORT void api_update( BLINK_API* api );
+	BLINK_API_EXPORT bool api_exit( BLINK_API* api );
 }
